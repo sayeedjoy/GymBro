9ce8f9537f006e6dc607d4fde68fac0ca1e70fc0
Sayeed Joy <sayeedalmahmudjoy@gmail.com>
2025-11-02 23:51:29 +0600

    New: Add WeightRecordCard composable
    Adds a new, empty `WeightRecordCard` composable function to the `WeightScreen.kt` file.

--- app/src/main/java/com/sayeedjoy/gymbro/ui/screens/WeightScreen.kt
+++ app/src/main/java/com/sayeedjoy/gymbro/ui/screens/WeightScreen.kt
@@ -1,7 +1,22 @@
 package com.sayeedjoy.gymbro.ui.screens
 
-import androidx.compose.foundation.layout.*
-import androidx.compose.material3.*
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.WindowInsets
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedButton
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -62,6 +77,11 @@ fun WeightScreen() {
             }
         }
     }
+}
+
+@Composable
+fun WeightRecordCard(){
+
 }
 
 @Preview(showBackground = true)
